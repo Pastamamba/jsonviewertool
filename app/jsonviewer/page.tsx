a9c9deca4716c40ac65ae7609fd89e80bea04da7
Petri <35950897+Pastamamba@users.noreply.github.com>
2024-05-02 09:15:24 +0300

    Project set up

---
+++ app/jsonviewer/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {SidePanel} from "@/components/sidepanel/SidePanel";
+
+const JsonviewerPage: React.FC = () => {
+  return (
+    <>
+        <h1>JsonViewer component</h1>
+        <SidePanel />
+    </>
+  );
+}
+
+export default JsonviewerPage;
---
+++ components/sidepanel/SidePanel.tsx
@@ -0,0 +1,7 @@
+export const SidePanel = () => {
+  return (
+    <div>
+      <h1>SidePanel</h1>
+    </div>
+  );
+}
